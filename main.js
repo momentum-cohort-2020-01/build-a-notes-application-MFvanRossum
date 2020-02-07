bca13d5f7b255d29964e0e95f81cbabf61e3d3c7
Amy Gori <amydgori@gmail.com>
2020-02-07 15:27:58 -0500

    Debugging and reformatting

--- main.js
+++ main.js
@@ -1,66 +1,66 @@
-function print(value) {
-    console.log(value)
-    return value
+function print (value) {
+  console.log(value)
+  return value
 }
 
-function qs(selector) {
-    return document.querySelector(selector)
+function qs (selector) {
+  return document.querySelector(selector)
 }
 
-function getAllNotes() {
-    return fetch('http://localhost:3000/notes/', {
-        method: 'GET'
-    })
-    .then(response => response.json())
+function getAllNotes () {
+  return fetch('http://localhost:3000/notes/', {
+    method: 'GET'
+  }).then(response => response.json())
 }
 
-function createNotesHTML(notes) {
-    let notesStr = '<div id="notes-list">'
-    for (note of notes) {
-        notesStr += createNoteHTML(note)
-    }
-    notesStr += '</div>'
-    return notesStr
+function createNotesHTML (notes) {
+  console.log('createNotesHTML', notes)
+  let notesStr = '<div id="notes-list">'
+  for (const note of notes) {
+    notesStr += createNoteHTML(note)
+  }
+  notesStr += '</div>'
+  return notesStr
 }
 
-function createNoteHTML(note) {
-    return `<div data-note-id="${note.id}">${note.note} <button class="delete">Delete</button></div>`
+function createNoteHTML (note) {
+  return `<div data-note-id="${note.id}">${note.note} <button class="delete">Delete</button></div>`
 }
 
-function postNewNote(noteText) {
-    return fetch('http://localhost:3000/notes/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json '},
-        body: JSON.stringify({ note: noteText, done: false })
-    })
-    .then(response => response.json())
+function postNewNote (noteText) {
+  return fetch('http://localhost:3000/notes/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json ' },
+    body: JSON.stringify({ note: noteText, done: false })
+  }).then(response => response.json())
 }
 
-function renderNotesList(notes) {
-    const notesHTML = createNotesHTML(notes)
-    const notesSection = qs('#notes')
-    notesSection.innerHTML = notesHTML
+function renderNotesList (notes) {
+  console.log('renderNotesList', notes)
+  const notesHTML = createNotesHTML(notes)
+  const notesSection = qs('#notes')
+  notesSection.innerHTML = notesHTML
 }
 
-function renderNewNote(note) {
-    const noteHTML = createNoteHTML(note)
-    const notesList = qs('#notes-list')
-    notesList.insertAdjacentHTML('beforeend', noteHTML)
+function renderNewNote (note) {
+  const noteHTML = createNoteHTML(note)
+  const notesList = qs('#notes-list')
+  notesList.insertAdjacentHTML('beforeend', noteHTML)
 }
 
 getAllNotes().then(renderNotesList)
 
 qs('#new-notes-form').addEventListener('submit', event => {
-    event.preventDefault()
-    const noteTitleField = qs('#note-title')
-    const noteTextField = qs('#note-text')
-    const noteText = noteTitleField.value + '<br>' + noteTextField.value
-    noteTextField.value = ''
-    postNewNote(noteText).then(renderNewNote)
+  event.preventDefault()
+  const noteTitleField = qs('#note-title')
+  const noteTextField = qs('#note-text')
+  const noteText = noteTitleField.value + '<br>' + noteTextField.value
+  noteTextField.value = ''
+  postNewNote(noteText).then(renderNewNote)
 })
 
 qs('#notes').addEventListener('click', event => {
-    if (event.target.matches('.delete')) {
-        print('delete ' + event.target.parentElement.dataset.noteID)
-    } 
+  if (event.target.matches('.delete')) {
+    print('delete ' + event.target.parentElement.dataset.noteID)
+  }
 })
